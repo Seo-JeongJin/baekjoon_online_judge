e4436df706e71f2ef2a2aa5ed0cf858869693e32
Seo JeongJin <tj0wjd9wls8@gmail.com>
2025-08-13 17:56:04 +0900

    solved in 250813

---
+++ 250813-1158.py
@@ -0,0 +1,15 @@
+
+from collections import deque
+
+n, k = map(int, input().split())
+
+arr = deque(range(1, n+1))
+
+result = []
+
+while arr:
+    for _ in range(k-1):
+        arr.append(arr.popleft())
+    result.append(arr.popleft())
+
+print("<" + ", ".join(map(str, result)) + ">")
