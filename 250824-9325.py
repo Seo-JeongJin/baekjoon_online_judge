09f82aa2a6337bbd0732a6e032e1926270ffc82f
Seo-JeongJin <tj0wjd9wls8@gmail.com>
2025-08-24 23:54:26 +0900

    solved in 250824

---
+++ 250824-9325.py
@@ -0,0 +1,10 @@
+
+t = int(input())
+
+for _ in range(t):
+    s = int(input())
+    n = int(input()) 
+    for _ in range(n):
+        q, p = map(int, input().split())
+        s += q * p
+    print(s)
